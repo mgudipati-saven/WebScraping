3a65152023d68efd4f247f6e727bc42ba153a294
Murthy Gudipati <mgudipati@saventech.com>
2013-03-05 17:06:19 +0530

    db schema changes
    - added index, date columns

--- bel20.rb
+++ bel20.rb
@@ -5,11 +5,15 @@ require 'nokogiri'
 require 'open-uri'
 require 'sqlite3'
 
-$dbname = 'bel20.sqlite'
-File.delete($dbname) if File.exists?$dbname
-$db = SQLite3::Database.new($dbname)
-$db.execute "CREATE TABLE BEL20_INDEX(`isin` VARCHAR, `symbol` VARCHAR, `name` VARCHAR)"
+$db = SQLite3::Database.new('ic.sqlite')
+$db.execute "CREATE TABLE IF NOT EXISTS INDEX_CONSTITUENTS(
+  `index` VARCHAR(16) NOT NULL,
+  `isin` VARCHAR(12) NOT NULL, 
+  `symbol` VARCHAR(10) NOT NULL, 
+  `name` VARCHAR,
+  `date` DATE NOT NULL)"
 
+index = "BEL20"
 page = Nokogiri::HTML(open('https://indices.nyx.com/en/products/indices/BE0389555039-XBRU'))
 rows = page.css('div.index-composition-block table tr')[1..-1]
 #puts "Number of rows: #{rows.length}"
@@ -19,10 +23,11 @@ rows.each do |row|
   name = row.css('td')[0].text.strip
   isin = row.css('td')[1].text.strip
   if isin
-    url = 
     page = Nokogiri::HTML(open("https://europeanequities.nyx.com/en/nyx_eu_listings/real-time/quote?isin=#{isin}&mic=XBRU"))
     symbol = page.css('div.first-row div.first-column.box-column').text.strip
     puts "#{name},#{isin},#{symbol}"
-    $db.execute "INSERT INTO BEL20_INDEX VALUES (?,?,?)", [isin, symbol, name]
+    if symbol
+      $db.execute "INSERT INTO INDEX_CONSTITUENTS VALUES (?,?,?,?,DATE('NOW'))", [index, isin, symbol, name]
+    end
   end
 end
