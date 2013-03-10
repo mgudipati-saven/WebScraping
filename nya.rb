7c9ce208efe833e26f136f0f86c33455d9b2a6d3
Murthy Gudipati <mgudipati@saventech.com>
2013-03-10 22:26:08 +0530

    added nyse composite index

---
+++ nya.rb
@@ -0,0 +1,27 @@
+# Scrape NYSE Composite Index Constituents
+require 'rubygems'
+require 'open-uri'
+require 'nokogiri'
+require 'sqlite3'
+require 'csv'
+
+$db = SQLite3::Database.new('ic.sqlite')
+$db.execute "CREATE TABLE IF NOT EXISTS IndexConstituents(
+  [index] VARCHAR(16) NOT NULL,
+  isin    VARCHAR(12), 
+  ticker  VARCHAR(16),
+  name    VARCHAR,
+  wt      FLOAT,
+  asof    DATE DEFAULT (date('now')))"
+
+index = 'NYA'
+isin = ''
+wt = '0.0'
+CSV.new(open("http://www.nyse.com/indexes/nyaindex.csv"), :headers => :first_row).each do |line|
+  name = line[0].strip
+  ticker = line[1].strip
+  if name != 'NAME' and ticker != 'TICKER'
+    puts "#{index},#{isin},#{name},#{ticker},#{wt}"
+    $db.execute "INSERT INTO IndexConstituents ([index], isin, ticker, name, wt) VALUES (?,?,?,?,?)", [index, isin, ticker, name, wt.to_f]  
+  end
+end
